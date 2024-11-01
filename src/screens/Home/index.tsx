fe1f1b30114a5a27e378024e63e549cd443c8824
Leonardo de Faveri <faver_i@hotmail.com>
2024-11-01 10:39:34 -0300

    chore: initial configs

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+import Logo from "../../assets/Logo.svg";
+import React from "react";
+
+export default function Home() {
+  return (
+    <View>
+      <Logo />
+    </View>
+  );
+}
